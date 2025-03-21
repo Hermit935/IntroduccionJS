bf2beca53ba245395e5d398a9899e6a8d9f57b23
Hiram Granados <xgranadosdhiramx@gmail.com>
2025-03-20 17:58:50 -0700

    20/03/25

--- 27.js
+++ 27.js
@@ -10,10 +10,33 @@ const producto = {
 
 function Producto (nombre, precio) {
     this.nombre = nombre;
-    this.precio = precio;
+    this.precio = precio; 
+}
+
+function Cliente (nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
 }
 const producto2 = new Producto('Monitor curvo de 49"', 800);
 const producto3 = new Producto('Laptop', 8000);
 
+const cliente = new Cliente('Hiram','Granados');
+
+console.log(cliente);
+
+function formatearProducto(producto ) {
+    return `El producto ${producto.nombre} tiene un precio de: $ ${producto.precio}`;
+}
+
 console.log(producto2);
 console.log(producto3);
+
+console.log(formatearProducto(producto2));
+console.log(formatearProducto(cliente));
+
+
+//Crear funciones que solo se utilizan en un objeto en espesifico
+
+Producto.prototype.formatearProducto = function() {
+    return `El producto ${producto.nombre} tiene un precio de: $ ${producto.precio}`;
+}
